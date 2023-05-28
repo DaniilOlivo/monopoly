e8f03535e1838f29d480c5cb4d83be75cd08b830
Danilka <mister.danial.olivo@gmail.com>
2023-05-28 18:19:00 +0300

    Add button update on Join List

--- client/src/Menu/JoinList/index.jsx
+++ client/src/Menu/JoinList/index.jsx
@@ -4,28 +4,31 @@ import Window from "../../components/Window"
 import { ListExtend } from "../../components/List"
 import Button from "../../components/Button"
 
+import "./JoinList.css"
+
 function JoinList(props) {
     // props
     // go - func transition between pages menu
     // startGameModule - func(room)
     let [rooms, setRooms] = useState({})
 
-    useEffect(() => {
-        async function getRooms() {
-            const response = await fetch("/api/listRooms")
-            const dataRooms = await response.json()
-            console.log(dataRooms)
-            const objRooms = {}
-            for (const [title, data] of Object.entries(dataRooms)) {
-                const { countPlayers } = data
+    async function getRooms() {
+        const response = await fetch("/api/listRooms")
+        const dataRooms = await response.json()
+        console.log(dataRooms)
+        const objRooms = {}
+        for (const [title, data] of Object.entries(dataRooms)) {
+            const { countPlayers } = data
 
-                objRooms[title] = {
-                    additionalTitle: `${countPlayers}/8`,
-                    handler: () => props.startGameModule(title)
-                }
+            objRooms[title] = {
+                additionalTitle: `${countPlayers}/8`,
+                handler: () => props.startGameModule(title)
             }
-            setRooms(objRooms)
         }
+        setRooms(objRooms)
+    }
+
+    useEffect(() => {
         getRooms()
     }, [])
 
@@ -33,7 +36,10 @@ function JoinList(props) {
         <Window>
             <div className="join-list">
                 < ListExtend title="Rooms" items={rooms} />
-                < Button title="Back" onClick={() => props.go("mainMenu")} />
+                <div className="join-list__panel-btns">
+                    < Button title="Back" onClick={() => props.go("mainMenu")} />
+                    < Button title="Update list" onClick={getRooms} />
+                </div>
             </div>
         </Window>
     )
