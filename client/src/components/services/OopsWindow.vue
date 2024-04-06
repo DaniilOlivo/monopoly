c5515d1d3c28e0c7a900abeafddd2ca78cd3c139
Daniil <mister.danial.olivo@gmail.com>
2024-04-06 20:31:28 +0300

    Add oops window

---
+++ client/src/components/services/OopsWindow.vue
@@ -0,0 +1,34 @@
+<template>
+    <WindowComponent :fullscreen="true" title="Oops. Something went wrong!">
+        <div class="oops-window">
+            <h1 class="oops-window__emotion">:(</h1>
+            <p class="oops-window__message">
+                {{ message }}
+            </p>
+        </div>
+    </WindowComponent>
+</template>
+
+<script>
+import WindowComponent from '@/components/common/WindowComponent.vue'
+
+export default {
+    name: "OopsWindow",
+    components: {
+        WindowComponent
+    },
+    props: {
+        message: String,
+    }
+}
+</script>
+
+<style scoped>
+.oops-window {
+    display: flex;
+}
+
+.oops-window__emotion {
+    font-size: 60px;
+}
+</style>
